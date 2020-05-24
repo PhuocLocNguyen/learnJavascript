ed9ee08bfde3e8eb4fdd76ac15af283370e5790b
PhuocLocNguyen <nguyenphuocloc095@gmail.com>
2020-05-24 11:41:42 +0700

    add task 24-scope

---
+++ 24-scope/index.js
@@ -0,0 +1,23 @@
+// var a = 1;
+
+// function foo() {
+//   var a = 2;
+//   return a;
+// }
+
+// function bar() {
+//   a = 2;
+//   return a;
+// }
+
+// foo();
+// console.log(a); // Kết quả? 1
+// bar();
+// console.log(a); // Kết quả? 2
+// // Giải thích vì sao kết quả dòng 14 lại khác 16 bằng tiếng Việt hoặc tiếng Anh
+// //Vì foo đã tạo ra 1 local scope nên chỉ trả về local scope
+// //Còn bar không tao ra local scope nên trả về global scope
+
+
+
+
